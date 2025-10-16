381ac9b3d89b44093cb3d5bb4bd291afbdb0944b
PabloRaka <ldeath665@gmail.com>
2025-10-16 17:52:44 +0700

    Update wsgi.py

--- gamestore/wsgi.py
+++ gamestore/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamestore.settings')
 
-app = get_wsgi_application() # <--- INI SOLUSINYA
+app = get_wsgi_application()
