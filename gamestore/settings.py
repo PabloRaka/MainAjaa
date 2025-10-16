3bc42002651c66fcc8883f8160094dfc914a583b
PabloRaka <ldeath665@gmail.com>
2025-10-17 00:15:45 +0700

    Update settings.py

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -82,9 +82,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'theme/static'),
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # ===============================================
 # TAILWIND & LAINNYA
