368431147cb44d2eed52ff813528d0650f297082
PabloRaka <ldeath665@gmail.com>
2025-10-16 15:19:30 +0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@
 # Keluar jika ada error
 set -e
 
-# Install dependencies
-pip install -r requirements.txt
+# Install dependencies (dengan perintah yang sudah benar)
+python3.9 -m pip install -r requirements.txt
 
 # Kumpulkan file statis
 python manage.py collectstatic --no-input
